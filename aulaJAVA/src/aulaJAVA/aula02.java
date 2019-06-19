096efdf6ca0889aab618585f9a80a80614709974
danyllo <danyllopenna10@gmail.com>
2019-06-19 15:04:24 -0300

    aula de loops e controle

--- aulaJAVA/src/aulaJAVA/aula02.java
+++ aulaJAVA/src/aulaJAVA/aula02.java
@@ -11,6 +11,29 @@ public class aula02 {
 			idade = idade + 1;
 			
 		}
+		
+		// for laço similar a o while com uma leitura mais simples
+		
+		for (int i = 0; i < 10 ; i++) {
+			
+			System.out.println(i);
+		}
+		
+		// controlando loops
+		int x = 0;
+		int y = 30;
+		
+		for (int i = x; i < y ; i++) {
+			if (i % 19 == 0 ) {
+				System.out.println("achei um numero divisivel por 19 "+ i);
+				break;
+				
+				
+			} else {
+
+			}
+			
+		}
 
 	}
 
