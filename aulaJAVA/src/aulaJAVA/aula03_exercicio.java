e9e7212e37f0cf4b11bb2236d222f8de88b913c7
danyllo <danyllopenna10@gmail.com>
2019-06-19 19:27:26 -0300

    exercicios de fixação

---
+++ aulaJAVA/src/aulaJAVA/aula03_exercicio.java
@@ -0,0 +1,56 @@
+package aulaJAVA;
+
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
+public class aula03_exercicio {
+
+	public static void main(String[] args) {
+		// Imprima todos os números de 150 a 300.
+		/*for (int i = 150; i <301; i++) {
+			System.out.println(i);
+			
+		}*/
+
+		
+		//Imprima a soma de 1 até 1000.
+	/*	int soma = 0;
+		 for (int i = 1; i < 10001; i++) {
+			 soma = soma + i;
+			 System.out.println(soma);
+			
+		}*/
+		
+		//Imprima todos os múltiplos de 3, entre 1 e 100.
+		/*for (int i = 0; i <=100 ; i++) {
+			if (i % 3 == 0) {
+				System.out.println("multiplo de 3:"+i);
+				
+			}*/
+		
+		//Imprima os fatoriais de 1 a 10.
+		
+		/*int fatorial = 1;
+		for (int i = 1; i <=10; i++) {
+			fatorial = fatorial * i;
+			System.out.println("fatorial de"+i+ "e = "+fatorial);
+			
+		} */
+		
+		
+			
+		}
+			
+		}
+
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+	
+	
+	
+
+
