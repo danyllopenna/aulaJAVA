0725c4dfc9982da90827ee88a861431975756e1d
danyllo <danyllopenna10@gmail.com>
2019-06-19 13:58:57 -0300

    aula 2 laços de repetição

---
+++ aulaJAVA/src/aulaJAVA/aula02.java
@@ -0,0 +1,17 @@
+package aulaJAVA;
+
+public class aula02 {
+
+	public static void main(String[] args) {
+		// aula de laços de repetição começamdo com while
+		
+		int idade = 1;
+		while (idade <= 18) {
+			System.out.println(idade);
+			idade = idade + 1;
+			
+		}
+
+	}
+
+}
